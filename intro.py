bf012e05bb95b7602a227ffab9d1b961f2642748
Ramesh Huliyar <ramhuliyar@gmail.com>
2021-06-26 12:44:53 +0530

    Tried the git add patch option

--- intro.py
+++ intro.py
@@ -3,6 +3,8 @@ print(sys.path)
 print("Hello Earth!")
 print("Hello Universe!")
 
+print("Trying git add patch option Line 1")
+
 print("Creating a Merge Conflict")
 print("Creating a Second Merge Conflict")
 
@@ -13,6 +15,8 @@ print("On temp branch")
 
 print("Changes done on GitHub on the temp branch")
 
+print("Trying git add patch option Line 2")
+
 print("Branch done on GitHub on the temp branch")
 
 print("Revert with files in working directory")
@@ -22,3 +26,5 @@ print("Making changes in the gitHub to perform fetch and merge operation")
 print("Fetch Understanding")
 
 print("Fetch Understanding 2")
+
+print("Trying git add patch option Line 3")
